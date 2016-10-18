c7e337c3fb301707aa127e458c1260671a260170
karthikthnr <karthikthnr@gmail.com>
2016-10-18 18:28:55 -0400

    part 2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ActiveRecord::Base
+    def Movie.all_ratings
+   	 Movie.uniq.pluck(:rating)
+    end
+    def self.filter(ratings)
+	if ratings != []
+	    return self.where(rating: ratings)
+	else
+	    return self.all
+	end
+    end
 end
